d8cb7caec8703cec18e02dd112788c2f1f1667a9
Baykov1337 <b3n.b4ykov@gmail.com>
2023-11-19 15:47:24 +0200

    lec2

--- test/Lection2/19112023.py
+++ test/Lection2/19112023.py
@@ -2,4 +2,10 @@ a = 1 # цяло число
 b = 3.14  #Плаваща запетая.
 c = True #bool
 text = "test" # дали ще е с единична или двойна кавичка 
+
+if a==b:
+ print ("test")
+else:
+   print ("ok" )
+   ##s
 print (type(b))
