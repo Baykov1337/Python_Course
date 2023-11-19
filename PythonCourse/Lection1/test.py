128230b46770a2c37ba88788c3f46a8b8c9f2121
Baykov1337 <b3n.b4ykov@gmail.com>
2023-11-19 17:03:41 +0200

    Lecture-end

--- PythonCourse/Lection1/test.py
+++ PythonCourse/Lection1/test.py
@@ -1,5 +1,14 @@
-print ("Hello Python attendant")
-print ("GitHub test!")
+#print ("GitHub test !")
+#print ("Hello Python attendant")
 
-test = 3+2
-print (test)
+#test = 3+2
+#print (test)
+
+str1 = 'test'
+str2 = 'new test'
+#print (f"{str1}\n{str2}")
+
+test_array = [1 ,2 ,3 ,4, 'abv', True]
+print(test_array[2])
+
+dict_array = {"a" : True , 'lastname' : 'Baykov', }
---
+++ PythonCourse/Lection2/sum_del__concat_mltply_arr.py
@@ -0,0 +1,33 @@
+#array 5 elements
+#dict 3 elements 
+
+num1 = 20
+num2 = 10 
+
+#sum 
+sum = num1 + num2 
+print (f'The sum of the numbers is :', sum)
+
+#del
+deletes = num1 - num2
+print (f'Result  {num1} delete {num2} = ', deletes)
+
+#devide 
+devide = num1 / num2
+print (f"Result {num1} devided to {num2} = " , devide)
+
+#concat 
+concat = str(num1) + str(num2)
+print (f"Concat between [{num1}] and [{num2}] is ",concat )
+
+#multiply 
+multiply = num1 * num2 
+print (f"[{num1}] multiplied to  [{num2}] is ",multiply )
+
+#array 
+small_array = ["fist", "second", "third", "forth", "five" ]
+print (small_array[4])
+
+#dict_array
+dict_array = {1 : "First", 2 : "Second", 3 : "Third"}
+print(dict_array[3])
