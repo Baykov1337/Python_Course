d0bd9d816bebcdc12838f5e5e4d5abe04c987b69
Ben Baykov <b.baykov@edynamix.com>
2023-11-21 17:38:02 +0200

    Little Format

--- Homework/HomeworkLection2.py
+++ Homework/HomeworkLection2.py
@@ -1,35 +1,37 @@
-#define 2 variable 
-#first one integer 
-#second one float 
-
-#devide first value to second
+#define 2 variable #first one integer #second one float 
+#divide first value to second
 
 val1 = 10
-val2 = 2.5
+val2 = 2.4
 
 #solution 
-devide = val1 / val2
-print (f"Devide [{val1}] / [{val2}] is :", devide)
+divide = val1 / val2
+print (f"Divide [{val1}] / [{val2}] is :", divide)
+
+#divide 2 values with result as whole number
+print (val1//val2)
 
 #concat 3 values - my fullname 
-
 fName = "Boyan"
 mName = "Hristov"
 lName = "Baykov"
-print (fName+" " +mName+" " +lName)
-
+print (fName+" " + mName + " " +lName)
+ 
 #Print each of my name in separate row with single print 
-print("My Full Name is :\n", fName, "\n",mName, "\n",lName )
+print("My Full Name is :\n", fName, "\n",mName, "\n", lName )
 
 #define 2 arrays - list and dictionary with at least 5 values 
-
 list_array = ["list1", "list2", "list3", "list4", "list5"]
-dict_array = {"val1" : "first", "val2":"second", "val3":"third", "val4":"forth", "val5":"fifth"}
+dict_array = {"val1" : "first", 
+              "val2":"second", 
+              "val3":"third", 
+              "val4":"forth", 
+              "val5":"fifth"}
 
-#print valuue before last one for each array
+#print value before last one for each array
 print (list_array[-2])
 
 #WIP - work in progress get the last dictionary word 
 #print (list(dict_array.keys())[-2])
-
+#the result
 print(dict_array[list(dict_array.keys())[-2]])
